5e309e28326183fd10434d94f8d8295e8ca2b354
Nathan Dang <nd432@cornell.edu>
2024-03-24 14:24:01 -0400

    Added candidate variant

--- final/lib/state.mli
+++ final/lib/state.mli
@@ -1,5 +1,8 @@
-type t = {name: string; votes: int; pop: int}
+type c
+(** [c] is a variant of possible candidates  *)
+type t = {name: string; votes: int; pop: int ; pref_can : c ; percent_win : float}
 (** [t] contains the basic instances of a State *)
+
 
 type d 
 (** [d] is the state-specific data. *)
