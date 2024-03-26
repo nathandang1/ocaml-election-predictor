11c27bb8d0b0028d25c00e6e7a06438c9e439c94
Andrew <ayk36@cornell.edu>
2024-03-26 14:56:05 -0400

    Create File module to extract data from files
    - Wrote implementation in file.ml
    - Wrote specifications in file.mli
    - Added 'csv' and 'batteries' as libraries in dune file

---
+++ final/lib/file.ml
@@ -0,0 +1,31 @@
+open Batteries
+
+module type ExtractableType = sig
+  type t
+
+  val from_csv : bool
+  val of_data : string list list -> t
+end
+
+module type F = sig
+  type t
+  type d
+
+  val of_name : string -> t
+  val extract : string -> d
+end
+
+module Make (Extr : ExtractableType) : F with type d = Extr.t = struct
+  type t = string
+  (** AF: The string [f] represents the file with the name [f]. RI: [f] must be
+      the name of a file that actually exists. *)
+
+  type d = Extr.t
+
+  let of_name (name : string) : t = name
+  let txt_data file = [ file |> BatFile.lines_of |> BatList.of_enum ]
+  let csv_data file : string list list = file |> Csv.load |> Csv.transpose
+
+  let extract file =
+    file |> (if Extr.from_csv then csv_data else txt_data) |> Extr.of_data
+end
---
+++ final/lib/file.mli
@@ -0,0 +1,35 @@
+(** An ExtractableType is a type of value that can be constructed using the data
+    extracted from either a .txt or a .csv file. *)
+module type ExtractableType = sig
+  type t
+  (** [t] represenets the type that can be constructed using data from a file. *)
+
+  val from_csv : bool
+  (** [from_csv] indicates whether [t] can be constructed from a .csv file. If
+      [from_csv] is false, then [t] should be constructable from a .txt file. *)
+
+  val of_data : string list list -> t
+  (** [of_data data] is the result when a nested list of strings is converted
+      into a value of type [t]. As a precondition, [of_data] can assume that
+      [data] contains a single list containing the rows of a .txt file if
+      [from_csv] is false, or that [data] contains the columns of a .csv file if
+      [from_csv] is true. *)
+end
+
+(** An F is a .txt or .csv file from which a certain data type can be extracted. *)
+module type F = sig
+  type t
+  (** [f] represents a .txt or .csv file. *)
+
+  type d
+  (** [d] represents the data type that can be extracted from a file. *)
+
+  val of_name : string -> t
+  (** [of_name name] is the file with the name [name]. *)
+
+  val extract : t -> d
+  (** [extract file] is the value of type [d] extracted from the file [file]. *)
+end
+
+(** [Make(Extr)] is the files from which values of type [Extr] can be extracted. *)
+module Make (Extr : ExtractableType) : F with type d = Extr.t
