8bc41093b3603e7098f0cefe1ab29bacaf02f386
Leon Jiao <lsj47@cornell.edu>
2024-03-26 10:54:43 -0400

    Add better functionality to State's outcome function
    - The function will now perform some computations to make a prediction
    - Modified state interface to have abstract representation types

--- final/lib/state.ml
+++ final/lib/state.ml
@@ -1,10 +1,26 @@
-type c = Biden | Trump
-type t = {name: string; votes: int; pop: int ; pref_can : c ; percent_win : float}
+type c =
+  | Biden
+  | Trump
 
-type d = float list
+type t = {
+  name : string;
+  votes : int;
+  pop : int;
+  pref_can : c;
+  pref_percent : float;
+}
 
-let outcome data = 
-  if data = [] then [0.]
-  else [0.5; 0.5]
+type d = c * float list
 
-
+let outcome state data =
+  let biden_val = List.assoc Biden data in
+  let trump_val = List.assoc Trump data in
+  let biden_boost =
+    if state.pref_can = Biden then state.pref_percent /. 5.
+    else -1. *. state.pref_percent /. 20.
+  in
+  let trump_boost =
+    if state.pref_can = Trump then state.pref_percent /. 5.
+    else -1. *. state.pref_percent /. 20.
+  in
+  [ (Biden, biden_val +. biden_boost); (Trump, trump_val +. trump_boost) ]
--- final/lib/state.mli
+++ final/lib/state.mli
@@ -1,11 +1,13 @@
 type c
-(** [c] is a variant of possible candidates  *)
-type t = {name: string; votes: int; pop: int ; pref_can : c ; percent_win : float}
+(** [c] is a variant of possible candidates *)
+
+type t
 (** [t] contains the basic instances of a State *)
+(* = {name: string; votes: int; pop: int ; pref_can : c ; percent_win :
+   float} *)
 
-
-type d 
+type d
 (** [d] is the state-specific data. *)
 
-val outcome : d -> float list
+val outcome : t -> d -> c * float list
 (** [outcome d] is each candidate's probability of winning given some data [d]. *)
