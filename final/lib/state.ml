b4e32e6e8736630233772d724b179c9711893824
Andrew <ayk36@cornell.edu>
2024-03-27 15:49:35 -0400

    Modify State module to be of ExtractableType
    - Re-implemented State within sub-module S
    - Added necessary functions for ExtractableType

--- final/lib/state.ml
+++ final/lib/state.ml
@@ -1,26 +1,42 @@
-type c =
-  | Biden
-  | Trump
+module S = struct
+  type c =
+    | Biden
+    | Trump
 
-type t = {
-  name : string;
-  votes : int;
-  pop : int;
-  pref_can : c;
-  pref_percent : float;
-}
+  type t = {
+    name : string;
+    votes : int;
+    pop : int;
+    pref_can : c;
+    pref_percent : float;
+  }
 
-type d = (c * float) list
+  type d = (c * float) list
 
-let outcome state (data : d) =
-  let biden_val = List.assoc Biden data in
-  let trump_val = List.assoc Trump data in
-  let biden_boost =
-    if state.pref_can = Biden then state.pref_percent /. 5.
-    else -1. *. state.pref_percent /. 20.
-  in
-  let trump_boost =
-    if state.pref_can = Trump then state.pref_percent /. 5.
-    else -1. *. state.pref_percent /. 20.
-  in
-  [ (Biden, biden_val +. biden_boost); (Trump, trump_val +. trump_boost) ]
+  let outcome state (data : d) =
+    let biden_val = List.assoc Biden data in
+    let trump_val = List.assoc Trump data in
+    let biden_boost =
+      if state.pref_can = Biden then state.pref_percent /. 5.
+      else -1. *. state.pref_percent /. 20.
+    in
+    let trump_boost =
+      if state.pref_can = Trump then state.pref_percent /. 5.
+      else -1. *. state.pref_percent /. 20.
+    in
+    [ (Biden, biden_val +. biden_boost); (Trump, trump_val +. trump_boost) ]
+
+  let from_csv = false
+
+  let of_data data =
+    match List.hd data with
+    | nm :: vts :: p :: c :: c_per :: _ ->
+        {
+          name = nm;
+          votes = int_of_string vts;
+          pop = int_of_string p;
+          pref_can = (if c = "Donald Trump" then Trump else Biden);
+          pref_percent = float_of_string c_per;
+        }
+    | _ -> failwith "invalid data"
+end
--- final/lib/state.mli
+++ final/lib/state.mli
@@ -1,13 +1,17 @@
-type c
-(** [c] is a variant of possible candidates *)
+(** An S is a state-level electoral subdivision of an electoral map. *)
+module S : sig
+  type c
+  (** [c] is a variant of possible candidates *)
 
-type t
-(** [t] contains the basic instances of a State *)
-(* = {name: string; votes: int; pop: int ; pref_can : c ; percent_win :
-   float} *)
+  type t
+  (** [t] contains the basic instances of a State *)
+  (* = {name: string; votes: int; pop: int ; pref_can : c ; percent_win :
+     float} *)
 
-type d
-(** [d] is the state-specific data. *)
+  type d
+  (** [d] is the state-specific data. *)
 
-val outcome : t -> d -> (c * float) list
-(** [outcome d] is each candidate's probability of winning given some data [d]. *)
+  val outcome : t -> d -> (c * float) list
+  (** [outcome d] is each candidate's probability of winning given some data
+      [d]. *)
+end
