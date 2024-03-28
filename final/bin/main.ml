5ea90b7d372187c2531dce2dcdb93729c067e0af
Nathan Dang <nd432@cornell.edu>
2024-03-28 16:28:05 -0400

    deleted run()

--- final/bin/main.ml
+++ final/bin/main.ml
@@ -28,17 +28,3 @@ module ExtractStates = GroupExtract (StateFile)
 
 let candidates : Candidate.C.t list = ExtractCandidates.from_lst candidate_files
 let states : State.S.t list = ExtractStates.from_lst state_files
-
-let run () =
-  print_endline "***** OCAML ELECTION PREDICTOR *****";
-  print_endline "";
-  print_endline
-    "[Cornell | 7] -> [Joe Biden: 22 votes] [Donald Trump: 12 votes]";
-  print_endline "[Harvard | 5] -> [Joe Biden: 22 votes] [Donald Trump: 8 votes]";
-  print_endline "[Yale | 3] -> [Joe Biden: 22 votes] [Donald Trump: 28 votes]";
-  print_endline "";
-  print_endline
-    "[OVERALL | 15] -> [Joe Biden: 66 votes, 12 electors] [Donald Trump: 48 \
-     votes, 3 electors]"
-
-let _ = run ()
