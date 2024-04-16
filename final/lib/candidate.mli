7303fb1ab381fa1a1df324a980b19406794fdcc0
Andrew <ayk36@cornell.edu>
2024-04-16 19:20:58 -0400

    Remove redundant code from Candidate and State modules
    - Removed encapsulating module in Candidate module
    - Removed encapsulating module in State module
    - Updated main.ml to reflect changes to Candidate and State modules

--- final/lib/candidate.mli
+++ final/lib/candidate.mli
@@ -1,27 +1,24 @@
-(** A C is a candidate for an election. *)
-module C : sig
-  type t = {
-    name : string;
-    party : string;
-  }
-  (** [t] represents a candidate in an election. *)
+type t = {
+  name : string;
+  party : string;
+}
+(** [t] represents a candidate in an election. *)
 
-  val create : string * string -> t
-  (** [create (nm, pty)] creates a candidate with name [nm] and party [pty]. *)
+val create : string * string -> t
+(** [create (nm, pty)] creates a candidate with name [nm] and party [pty]. *)
 
-  val name : t -> string
-  (** [name_of c] is the name of candidate [c]. *)
+val name : t -> string
+(** [name_of c] is the name of candidate [c]. *)
 
-  val party : t -> string
-  (** [party_of c] is the political party of candidate [c]. *)
+val party : t -> string
+(** [party_of c] is the political party of candidate [c]. *)
 
-  val from_csv : bool
-  (** [from_csv] indicates whether a candidate can be extracted from a .csv
-      file, and is [false].*)
+val from_csv : bool
+(** [from_csv] indicates whether a candidate can be extracted from a .csv file,
+    and is [false].*)
 
-  val of_data : string list list -> t
-  (** [of_data data] is the candidate created using information extracted from
-      the nested string list [data]. Requires that [data] contains a single
-      list, whose first element is the name of the candidate, and whose second
-      element is the name of the candidate's political party. *)
-end
+val of_data : string list list -> t
+(** [of_data data] is the candidate created using information extracted from the
+    nested string list [data]. Requires that [data] contains a single list,
+    whose first element is the name of the candidate, and whose second element
+    is the name of the candidate's political party. *)
--- final/lib/state.mli
+++ final/lib/state.mli
@@ -1,38 +1,35 @@
-(** An S is a state-level electoral subdivision of an electoral map. *)
-module S : sig
-  type c
-  (** [c] is a tuple of candidates for the election. *)
+type c
+(** [c] is a tuple of candidates for the election. *)
 
-  type t = {
-    name : string;  (** [name] is the name of the state *)
-    votes : int;  (** [votes] is the number of electoral votes the state has *)
-    pop : int;  (** [pop] is the population of the state *)
-    pref_can : string;
-        (** [pref_can] is the name of the candidate that the state prefers
-            currently *)
-    pref_percent : float;
-        (** [pref_percent] is the net advantage [pref_can] has over the
-            competition *)
-  }
-  (** [t] contains the basic instances of a State *)
-  (* = {name: string; votes: int; pop: int ; pref_can : c ; percent_win :
-     float} *)
+type t = {
+  name : string;  (** [name] is the name of the state *)
+  votes : int;  (** [votes] is the number of electoral votes the state has *)
+  pop : int;  (** [pop] is the population of the state *)
+  pref_can : string;
+      (** [pref_can] is the name of the candidate that the state prefers
+          currently *)
+  pref_percent : float;
+      (** [pref_percent] is the net advantage [pref_can] has over the
+          competition *)
+}
+(** [t] contains the basic instances of a State *)
+(* = {name: string; votes: int; pop: int ; pref_can : c ; percent_win :
+   float} *)
 
-  type d = (string * float) list
-  (** [d] is the state-specific data. *)
+type d = (string * float) list
+(** [d] is the state-specific data. *)
 
-  val outcome : t -> d -> string -> float -> d
-  (** [outcome s d cand prior] is each candidate's probability of winning state
-      [s] given some data [d] and user-specified prior belief [prior] applied to
-      candidate [cand].*)
+val outcome : t -> d -> string -> float -> d
+(** [outcome s d cand prior] is each candidate's probability of winning state
+    [s] given some data [d] and user-specified prior belief [prior] applied to
+    candidate [cand].*)
 
-  val from_csv : bool
-  (** [from_csv] indicates whether a state can be extracted from a .csv file,
-      and is [false].*)
+val from_csv : bool
+(** [from_csv] indicates whether a state can be extracted from a .csv file, and
+    is [false].*)
 
-  val of_data : string list list -> t
-  (** [of_data data] is the state created using information extracted from the
-      nested string list [data]. Requires that [data] contains a single list,
-      whose elements include the name of the state, its votes, its population,
-      its preferred candidate, and its preference percentage, in that order. *)
-end
+val of_data : string list list -> t
+(** [of_data data] is the state created using information extracted from the
+    nested string list [data]. Requires that [data] contains a single list,
+    whose elements include the name of the state, its votes, its population, its
+    preferred candidate, and its preference percentage, in that order. *)
