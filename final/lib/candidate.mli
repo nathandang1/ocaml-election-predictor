3f671fd4e32d08f821ba3a5c06dad3a714e3bad3
Leon Huang <lyh7@cornell.edu>
2024-05-15 11:44:12 -0400

    Fixed compile errors

--- final/lib/candidate.mli
+++ final/lib/candidate.mli
@@ -3,3 +3,9 @@ type t = {
   party : string;
 }
 (** [t] represents a candidate in an election. *)
+
+val create : string * string -> t
+
+val name : t -> string
+
+val party : t -> string
--- final/tests/test_final.ml
+++ final/tests/test_final.ml
@@ -3,5 +3,3 @@ open State_tests
 open Candidate_tests
 
 let () = run_statepoll_tests
-let () = run_state_tests
-let () = run_candidate_tests
