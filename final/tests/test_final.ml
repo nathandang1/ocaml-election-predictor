7ef3984e811ae51c54b490bc405a7750b99889e8
Nathan Dang <nd432@cornell.edu>
2024-05-16 03:57:33 -0400

    extractor_tests.ml

--- final/tests/test_final.ml
+++ final/tests/test_final.ml
@@ -3,9 +3,12 @@ open Candidate_tests
 open Countrypoll_tests
 open Models_tests
 open Model_test_broken
+open Extractor_tests
+open Regularizer_tests
 
 let () = run_statepoll_tests
 let () = run_candidate_test
 let () = run_countrypoll_tests
 let () = run_models_test
-let () = run_models_tests ()
+let () = run_extractor_test
+let () = run_regularizer_test
