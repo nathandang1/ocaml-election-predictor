4515dbeb73b68ae82a85c470d396829e6eee3c85
Nathan Dang <nd432@cornell.edu>
2024-05-16 11:10:22 -0400

    Documentation for candidate

--- final/lib/candidate.mli
+++ final/lib/candidate.mli
@@ -5,7 +5,10 @@ type t = {
 (** [t] represents a candidate in an election. *)
 
 val create : string * string -> t
+(**[create nme prty] creates a candidate of type [t] who's name = [nme] and party [prty]  *)
 
 val name : t -> string
-
+(**returns the [name] of a candidate *)
 val party : t -> string
+(**returns the [party] of a candidate *)
+
