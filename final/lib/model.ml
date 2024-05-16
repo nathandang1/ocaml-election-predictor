74c764e38cda7f728b37570e698911537818dec2
Andrew <ayk36@cornell.edu>
2024-05-15 20:17:25 -0400

    Add Bayes model to model pipeline
    - Added Naive Bayes to model pipeline

--- final/lib/model.ml
+++ final/lib/model.ml
@@ -1,13 +1,32 @@
-let run ((candidates : Candidate.t list), (state : State.t), polling) =
+type model =
+  | Uniform
+  | Bayes
+
+let uniform_model candidates =
+  List.map
+    (fun cand -> (cand, 1. /. float_of_int (List.length candidates)))
+    candidates
+
+let bayes_model (state : State.t) =
+  match
+    Models.naive_bayes_randomized
+      (List.tl
+         (Csv.load ("data/data-extraction/state-data/" ^ state.name ^ ".csv")))
+  with
+  | dem, rep ->
+      [
+        (Candidate.create ("Joe Biden", "Democrat"), dem);
+        (Candidate.create ("Donald Trump", "Republican"), rep);
+      ]
+
+let run ((candidates : Candidate.t list), (state : State.t), polling) model =
   let () = ignore polling in
-  (* INSERT MODEL HERE *)
   let probabilities =
-    (* TEMP IMPLEMENTATION: declares each candidate is equally likely to win. *)
-    List.map
-      (fun cand -> (cand, 1. /. float_of_int (List.length candidates)))
-      candidates
+    match model with
+    | Uniform -> uniform_model candidates
+    | Bayes -> bayes_model (state : State.t)
   in
   (state, probabilities)
 
-let run_all (candidates, states, polling) =
-  List.map (fun state -> run (candidates, state, polling)) states
+let run_all (candidates, states, polling) model =
+  List.map (fun state -> run (candidates, state, polling) model) states
--- final/lib/model.mli
+++ final/lib/model.mli
@@ -1,11 +1,19 @@
+type model =
+  | Uniform
+  | Bayes
+
 val run :
   Candidate.t list * State.t * (State.t * Csv.t) list ->
+  model ->
   State.t * (Candidate.t * float) list
-(** [run (candidates, state, polling)] is the probabilities that each candidate
-    in [candidates] wins in [state] given [polling]. *)
+(** [run (candidates, state, polling) model] is the probabilities that each
+    candidate in [candidates] wins in [state] given [polling], with model
+    [model]. *)
 
 val run_all :
   Candidate.t list * State.t list * (State.t * Csv.t) list ->
+  model ->
   (State.t * (Candidate.t * float) list) list
-(** [run (candidates, states, polling)] is the probabilities that each candidate
-    in [candidates] wins in each state in [states] given [polling]. *)
+(** [run (candidates, states, polling) model] is the probabilities that each
+    candidate in [candidates] wins in each state in [states] given [polling],
+    with model [model]. *)
