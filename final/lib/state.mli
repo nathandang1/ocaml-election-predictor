67abd2f49051dc2b2b3ffe408431156302a4df81
Nathan Dang <nd432@cornell.edu>
2024-05-16 15:12:27 -0400

    editted state.mli documentation

--- final/lib/state.mli
+++ final/lib/state.mli
@@ -3,11 +3,8 @@ type t = {
   abbr : string;  (** [abbr] is an abbreviation of the state's name. *)
   votes : int;  (** [votes] is the number of electoral votes the state has *)
   pop : int;  (** [pop] is the population of the state *)
-  pref_can : string;
-  pref_percent : float;
+  pref_can : string; (** [pref_can] is the preferred candidate of the state*)
+  pref_percent : float; (**[pref_percent] is the percentage for the preferred candidate of the state*)
 }
 (** [t] contains the basic metadata of a state *)
 
-(* type d = (string * float) list *)
-
-(* val outcome : t -> d -> string -> float -> d *)
